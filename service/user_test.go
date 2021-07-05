3c617f0e1847dbdc1436bcef2b3cd8a128e2f93e
Jinsu Park <dev.umijs@gmail.com>
2021-07-05 16:47:28 +0900

    test: IsAdmin

---
+++ service/user_test.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+    "github.com/khu-dev/khumu-club/data"
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestIsAdmin(t *testing.T){
+    user := &data.User{
+        Groups: []string{
+            "admin",
+        },
+    }
+    assert.True(t, IsAdmin(user))
+
+    userFromDto := data.MapUserDto2User(&data.UserDto{
+        Username: "testuser",
+        Groups: []*data.Group{
+            &data.Group{
+                ID: 1,
+                Name: "admin",
+            },
+        },
+    })
+
+    assert.True(t, IsAdmin(userFromDto))
+}
